71060fbf9470a674681651df621088940f59eda7
AnukritiKathane <85435427+AnukritiKathane@users.noreply.github.com>
2021-06-06 18:07:24 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer()
+{
+  return (
+    <footer>
+      <h1> copyright @ 2021 </h1>
+      </footer>
+  );
+}
+export default Footer;
---
+++ Note.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+function Note()
+{
+  return (
+    <div className = "note">
+    <h1> <ul>JAVASCRIPT & REACT.JS </ul></h1>
+    <p> <h4>This 7 days bootcamp was really amazing that is conducted by institution ShapeAI by our mentor Shaurya Sinha sir.He has guided everything that is required for a fresher
+      </h4> </p>
+     <p> <h4> I learned everything from scratch including Javascript,React.js,HTML. </h4> </p> 
+     <p> <h4> I imbibe a loads of things from Shaurya sir without spending a single penny.</h4></p>
+     
+     <p> <h3> THANK YOU SO MUCH SHAURYA SINHA SIR:)))</h3></p>
+     </div> 
+
+  )
+}
+export default Note;
